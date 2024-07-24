a341b1a29c66565a32a354eb5ad30860995f428c
carlosharo-admin <carlos.haro@jakaysa.com>
2024-07-24 12:44:08 -0500

    Activated Actions server. Include an example story that uses a service from actions to retrieve the time zone of a region. Added twilio certificates to itegrate whatsapp to the chatbot. axcluded the visible credential.

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,48 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import requests
+BASE_URL = "http://worldtimeapi.org/api/timezone/"
+class ActionShowTimeZone(Action):
+    """
+    every class has just 2 methods: name & run
+    """
+    def name(self) -> Text:
+        """Returns the name of action
+
+        Returns:
+            Text: the text we will register in `domain.py`
+        """
+        return "action_find_timezone"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # we can get values from slots by `tracker` object
+        target_timezone = tracker.get_slot('target_timezone')
+
+        try:
+            res = requests.get(BASE_URL + target_timezone)
+            res.raise_for_status()  # Raise an exception for HTTP errors
+            res_json = res.json()
+
+            if 'utc_offset' in res_json:
+                output = f"Time zone is {res_json['utc_offset']}"
+            else:
+                output = "Please type in this structure: Area/Region"
+        except requests.exceptions.HTTPError:
+            output = 'Ops! There are too many requests on the time zone API. Please try a few moments later...'
+        except requests.exceptions.RequestException as e:
+            output = f"Request error: {e}"
+        except Exception as e:
+            output = f"An error occurred: {e}"
+
+        dispatcher.utter_message(text=output)
+
+        return []
